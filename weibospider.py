62ff3442b73c2a469c44dfc9e3e1a52ad8b7373d
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-04-27 20:29:46 +0800

    Update weibospider.py

--- weibospider.py
+++ weibospider.py
@@ -1,35 +1,84 @@
 # -*- coding: utf-8 -*-
-import random
+import datetime
 import json
+import math
+import os
+import random
+import requests
 import sys
 import time
-import requests
-import os
-import datetime
 
 
-def get_headers() -> dict:
-    return {'User-Agent': get_user_agent()}
+class WeiboSpider(object):
+    def __init__(self, user_id: str):
+        self.user_id: str = user_id
+        self.user_info: dict = {}
+        self.weibo_count: int = 0
+        self.page_count: int = 0
+        self.weibo_container_id: str = ''
 
 
-def get_user_agent() -> str:
-    first_num = random.randint(55, 76)
-    third_num = random.randint(0, 3800)
-    fourth_num = random.randint(0, 140)
-    operation_system_type = [
-        '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
-        '(Macintosh; Intel Mac OS X 10_14_5)'
-    ]
-    chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
+    def get_data(self,url:str):
+        data = requests.get(url=url).text
+        return data
+    def get_container_id(self, info):
+        data = self.get_data(url)
+        content = json.loads(data).get('data')
+        for tab in content.get('tabsInfo').get('tabs'):
+            if tab.get('tab_type') == 'weibo':
+                self.weibo_container_id = tab.get('containerid')
 
-    user_agent = ' '.join(['Mozilla/5.0', random.choice(operation_system_type), 'AppleWebKit/537.36',
-                           '(KHTML, like Gecko)', chrome_version, 'Safari/537.36'])
-    return user_agent
+    def get_weibo_and_page_count(self):
+        weibo_count = self.user['statuses_count']
+        self.page_count = int(math.ceil(weibo_count / 10.0))
+
+    def get_user_info(self):
+        url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=' + self.user_id
+        data = self.get_data(url)
+        user_info = json.loads(data).get('data').get('userInfo')
+        self.user_info = {'screen_name': user_info.get('screen_name'),
+                          'profile_url': user_info.get('profile_url'),
+                          'statuses_count': user_info.get('statuses_count'),
+                          'verified_reason': user_info.get('verified_reason'),
+                          'description': user_info.get('description'),
+                          'follow_count': user_info.get('follow_count'),
+                          'followers_count_str': user_info.get('followers_count_str')}
+        self.get_page_count()
+        self.get_container_id()
 
 
-def get_data(url) -> str:
-    data = requests.get(url=url, headers=get_headers()).text
-    return data
+
+
+    def get_weibo:
+
+
+    def get_picture_info(self):
+        weibo_url_prefix = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=' + self.user_id + '&containerid=' + self.weibo_container_id + '&page='
+        for i in range(0, self.page_count):
+            weibo_url = weibo_url_prefix + str(i)
+            try:
+                data = get_data(weibo_url)
+                content = json.loads(data).get('data')
+                cards = content.get('cards')
+
+
+
+    def download_picture(self, url, file_path='./img'):
+        if not os.path.isdir(file_path):
+            os.makedirs(file_path)
+        try:
+
+        except Exception as e:
+
+
+    def markdown(self):
+
+    def run(self):
+        self.get_user_info()
+
+
+
+
 
 
 def get_container_id(url) -> str:
@@ -39,19 +88,6 @@ def get_container_id(url) -> str:
         if tab.get('tab_type') == 'weibo':
             container_id = tab.get('containerid')
     return container_id
-
-
-def get_user_info(user_id) -> dict:
-    url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=' + user_id
-    data = get_data(url)
-    user_info = json.loads(data).get('data').get('userInfo')
-    user_info = {'screen_name': user_info.get('screen_name'),
-                 'profile_url': user_info.get('profile_url'),
-                 'verified_reason': user_info.get('verified_reason'),
-                 'description': user_info.get('description'),
-                 'follow_count': user_info.get('follow_count'),
-                 'followers_count': user_info.get('followers_count')}
-    return user_info
 
 
 def get_picture(user_id, file_path):
@@ -104,26 +140,19 @@ def get_picture(user_id, file_path):
                             "点赞数：" + str(attitudes_count) + "\n" +
                             "评论数：" + str(comments_count) + "\n" +
                             "转发数：" + str(reposts_count) + "\n")
-                        markdown(file_path,
-                                 '# ' + img_time + '\n'
-                                                   '![](' + './' + img_name + ')'+'\n'
-                                 )
             else:
                 break
         except BaseException as e:
-            log(file_path, '\n'+'***error***: ' + str(e) +  '\n')
+            log(file_path, '\n' + '***error***: ' + str(e) + '\n')
             pass
 
-
-def markdown(file_path, str=""):
-    with open(file_path + "weibo.md", 'a', encoding='utf-8') as f:
-        f.write(str)
 
 
 def log(file_path, str=""):
     with open(file_path + "log.txt", 'a', encoding='utf-8') as f:
         f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '\n')
         f.write(str)
+
 
 def get_latest_file(directory):
     latest_file = None
@@ -140,15 +169,42 @@ def get_latest_file(directory):
     return latest_file
 
 
-
 if __name__ == "__main__":
-    id = '2019071187'
-    user_info = get_user_info(id)
-    file_path = ".\\" + user_info["screen_name"] + "\\"
-    if not os.path.isdir(file_path):
-        os.mkdir(file_path)
-    log(file_path, str(user_info))
-    get_picture(id, file_path)
+    wb = WeiboSpider('2019071187')
+    wb.run()
+
+    # id = '2019071187'
+    # user_info = get_user_info(id)
+    # file_path = ".\\" + user_info["screen_name"] + "\\"
+    # if not os.path.isdir(file_path):
+    #     os.mkdir(file_path)
+    # log(file_path, str(user_info))
+    # get_picture(id, file_path)
+
+
+
+
+
+
+
+# def get_headers() -> dict:
+#     return {'User-Agent': get_user_agent()}
+#
+
+# def get_user_agent() -> str:
+#     first_num = random.randint(55, 76)
+#     third_num = random.randint(0, 3800)
+#     fourth_num = random.randint(0, 140)
+#     operation_system_type = [
+#         '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
+#         '(Macintosh; Intel Mac OS X 10_14_5)'
+#     ]
+#     chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
+#
+#     user_agent = ' '.join(['Mozilla/5.0', random.choice(operation_system_type), 'AppleWebKit/537.36',
+#                            '(KHTML, like Gecko)', chrome_version, 'Safari/537.36'])
+#     return user_agent
+
 
 # def get_proxy_ip_list() -> list[dict[str, str]]:
 #     proxy_ip_list: list[dict[str, str]] = []
